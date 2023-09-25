c8da2d533914c897f085f12648ca4b541ffe33de
dreymotoh <striclyspoken@gmail.com>
2023-09-25 17:24:32 -0400

    ?

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 // // Applies next-auth to whole site
+// // Applies next-auth to whole site
 export { default } from "next-auth/middleware"
 
-// // Allows public access to homepage and registration
-// export const config = {matcher: ['']}
+// Allows public access to homepage and registration
+export const config = {matcher: ['/']}
